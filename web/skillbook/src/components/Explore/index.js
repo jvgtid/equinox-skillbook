468febb7f500295f3e63432f588d0d6f6331c7f5
Hugo Ferrando Seage <hugoseage@gmail.com>
2019-03-28 16:32:27 +0100

    Add Header button logic

---
+++ web/skillbook/src/components/Explore/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+
+class Explore extends React.Component {
+    state = {
+
+    };
+
+    render() {
+        return (
+            <div>{'TEST TEXT'}</div>
+        );
+    }
+}
+
+export default Explore;
--- web/skillbook/src/components/Header/index.js
+++ web/skillbook/src/components/Header/index.js
@@ -3,16 +3,34 @@ import './index.css';
 
 class Header extends React.Component {
     state = {
-
+        homeStyle: {
+            backgroundColor: '#6e6e6e',
+        },
+        exploreStyle: {
+            backgroundColor: '#5e5e5e',
+        }
     };
+
+    changeView = (name) => {
+        this.props.changeView(name);
+        // ugly
+        this.setState({
+            homeStyle: {
+                backgroundColor: name === 'home' ? '#6e6e6e' : '#5e5e5e',
+            },
+            exploreStyle: {
+                backgroundColor: name === 'explore' ? '#6e6e6e' : '#5e5e5e',
+            }
+        })
+    }
 
     render() {
         return (
-            <div className={ 'header-container flex-horizontal'}>
-                <div className={'header-button'}>
+            <div className={'header-container flex-horizontal'}>
+                <div id='home' className={'header-button'} style={this.state.homeStyle} onClick={() => {return this.changeView('home')}}>
                     Inicio
                 </div>
-                <div className={'header-button'}>
+                <div id='explore' className={'header-button'} style={this.state.exploreStyle} onClick={() => {return this.changeView('explore')}}>
                     Explorar
                 </div>
             </div>
