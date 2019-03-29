48fbd3cbc0d47b7c5ca7c850bc083243baac9634
mgs95 <mariano.gonzalezsalazar@telefonica.com>
2019-03-29 02:49:10 +0100

    landing

--- web/skillbook/src/components/Explore/People/PeopleBox/index.js
+++ web/skillbook/src/components/Explore/People/PeopleBox/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 import Icon from '../../../Icon';
 import { ICONS } from "../../../../constants";
 import './index.css';
+import Skills from "../../../Sidebar/Skills";
+import Badges from "../../../Sidebar/Badges";
+
+const skills = [
+    { name: 'Javascript', score: 9 },
+    { name: 'Python', score: 7 },
+    { name: 'React', score: 7 },
+    { name: 'Django', score: 6 },
+    { name: 'Scala', score: 2 },
+    { name: 'Spark', score: 2 },
+    { name: 'Elastic Search', score: 1 },
+];
+const badges = [
+    { type: 'general', color: '#005a74', icon: 'JS', title: 'Mejor programador de Javascript' },
+    { type: 'general', color: '#005a74', icon: 'Py', title: 'Mejor programador de Python' },
+    { type: 'expertise', color: '#80a696', icon: 'V', title: 'Veterano de Telefonica' },
+];
+
 
 class PeopleBox extends React.Component {
     state = {
@@ -15,7 +33,18 @@ class PeopleBox extends React.Component {
     render() {
         let userInfo = '';
         if(this.state.showInfo) {
-            userInfo = <div>AAAAAAAAAAAAAAAAAAAAAAA</div>;
+            userInfo = (
+                <div className={'user-profile-container flex-vertical'}>
+                    <div className={ 'user-img' } style={{ height: 300, width: 300}}>
+                        <div className={'img'}></div>
+                    </div>
+                    <div className={'user-name'}>
+                        { 'MARIANO GONZALEZ SALAZAR' }
+                    </div>
+                    <Skills data={ skills }/>
+                    <Badges data={ badges }/>
+                </div>
+            );
         }
 
         return (
